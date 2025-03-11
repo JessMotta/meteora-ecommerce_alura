a5aa39789bcb4110a21a275541ae0e06fbcf9bcb
Jéssica Motta <jessicalimamotta@gmail.com>
2025-03-11 18:13:48 -0300

    add all categories

--- src/components/CardCat/index.tsx
+++ src/components/CardCat/index.tsx
@@ -1,18 +1,51 @@
 import Image from "next/image";
 
+// colocar esse array dentro do mock
+const categories = [
+    {
+        title: 'Camisetas',
+        image: '/products/catCamiseta.jpg',
+        alt: 'Imagem de camiseta',
+    },
+    {
+        title: 'Bolsas',
+        image: '/products/catBolsa.jpg',
+        alt: 'Imagem de bolsa',
+    },
+    {
+        title: 'Calçados',
+        image: '/products/catCalcado.jpg',
+        alt: 'Imagem de calçado',
+    },
+    {
+        title: 'Calças',
+        image: '/products/catCalca.jpg',
+        alt: 'Imagem de calça',
+    },
+    {
+        title: 'Casacos',
+        image: '/products/catCasaco.jpg',
+        alt: 'Imagem de casaco',
+    },
+    {
+        title: 'Óculos',
+        image: '/products/catOculos.jpg',
+        alt: 'Imagem de óculos',
+    }
+]
+
 export function CardCat() {
     return (
         <div className="text-center font-medium">
             <h2 className="text-[32px]">Busque por categoria:</h2>
-            <div className="flex justify-start gap-[30px] mx-[165px] mt-8 mb-11">
-                <div className="max-w-40">
-                    <Image src="/products/catCamiseta.jpg" width={160} height={72} alt='Categoria camisetas' className='mr-6' />
-                    <p className="text-white bg-black py-2">Camisetas</p>
+            <div className="flex justify-center gap-[30px] mt-8 mb-11">
+                {categories.map((category, index) => (
+                    <div key={index} className="max-w-40">
+                    <Image src={category.image} width={160} height={72} alt={category.alt} className='mr-6' />
+                    <p className="text-white bg-black py-2">{category.title}</p>
                 </div>
-                <div className="max-w-40">
-                    <Image src="/products/catCamiseta.jpg" width={160} height={72} alt='Categoria camisetas' className='mr-6' />
-                    <p className="text-white bg-black py-2">Camisetas</p>
-                </div>
+                ))}
+                
             </div>
         </div>
 
