5825e82619ea297cdd08399aa2e13a32560163e4
Jéssica Motta <jessicalimamotta@gmail.com>
2025-03-11 17:46:10 -0300

    adjust footer

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -6,7 +6,7 @@ import Form from "next/form";
 
 export function Newsletter() {
     return (
-        <div className="w-full fixed bottom-[124px]">
+        <div className="w-full my-[60px]">
             <div className="border-2 border-black w-[730px] px-4 py-10 text-center mx-auto">
                 <p className="text-xl pb-6">Quer receber nossas novidades, promoções exclusivas e 10% OFF na primeira compra? <strong>Cadastre-se! </strong></p>
                 <Form action="/">
