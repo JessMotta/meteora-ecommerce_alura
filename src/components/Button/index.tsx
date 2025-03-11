2ece305e7ebede155cf2559f7deea03fbc603c8d
Jéssica Motta <jessicalimamotta@gmail.com>
2025-03-11 09:30:53 -0300

    fix button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { cn } from "@/helpers/cn";
 
 export default function Button({children, color = "purple"} : {children : React.ReactNode; color? : 'transparent' | 'purple'; className? : string}) {
-    const defaultButtonClasses = 'cursor-pointer border-1 border-white px-3 py-2 font-medium'
+    const defaultButtonClasses = 'cursor-pointer border-1 px-3 py-2 font-medium'
 
     const colors = {
-        transparent: 'bg-transparent text-white hover:text-(--color-primary) hover:border-(--color-primary)',
-        purple: 'bg-(--color-secondary) text-white hover:bg-white hover:text-(--color-secondary) hover:border-(--color-secondary)'
+        transparent: 'bg-transparent border-white text-white hover:text-(--color-primary) hover:border-(--color-primary)',
+        purple: 'bg-(--color-secondary) border-(--color-secondary) text-white hover:bg-white hover:text-(--color-secondary) hover:border-(--color-secondary)'
     }
     return (
         <button className={cn(defaultButtonClasses, colors[color])}
--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -11,7 +11,7 @@ export function Newsletter() {
                 <p className="text-xl pb-6">Quer receber nossas novidades, promoções exclusivas e 10% OFF na primeira compra? <strong>Cadastre-se! </strong></p>
                 <Form action="/">
                     <input type="text" placeholder="Digite seu e-mail" 
-                    className="border-1 border-black w-[456px] placeholder:text-(--color-gray) py-1.75 px-4"
+                    className="border-1 border-black w-[456px] placeholder:text-(--color-gray) py-2 px-4"
                     />
                     <Button>Enviar</Button>
                 </Form>
