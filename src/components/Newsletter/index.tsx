cfe0a42d97d99eb3f79237741c7600f5c14db734
Jéssica Motta <jessicalimamotta@gmail.com>
2025-04-01 19:58:41 -0300

    fix responsiveness newsletter

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -7,11 +7,11 @@ import Form from "next/form";
 export function Newsletter() {
     return (
         <div className="w-full my-[60px]">
-            <div className="border-2 border-black w-[730px] px-4 py-10 text-center mx-auto">
+            <div className="border-2 border-black max-w-[730px] px-4 py-10 text-center mx-auto">
                 <p className="text-xl pb-6">Quer receber nossas novidades, promoções exclusivas e 10% OFF na primeira compra? <strong>Cadastre-se! </strong></p>
                 <Form action="/">
                     <input type="text" placeholder="Digite seu e-mail" 
-                    className="border-1 border-black w-[456px] placeholder:text-(--color-gray) py-2 px-4"
+                    className="border-1 border-black lg:w-[456px] md:w-[350px] sm:w-[200px] placeholder:text-(--color-gray) py-2 px-4"
                     />
                     <Button>Enviar</Button>
                 </Form>
