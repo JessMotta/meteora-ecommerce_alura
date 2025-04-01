1db7244c600c020240d337ed4d777af86ff3254f
Jéssica Motta <jessicalimamotta@gmail.com>
2025-04-01 19:48:49 -0300

    fix products category section and titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ export default function Home() {
   return (
     <div>
       <div className="text-center font-medium my-18">
-        <h2 className="text-[32px]">Busque por categoria:</h2>
+        <h2 className="lg:text-[32px] md:text-2xl sm:text-xl">Busque por categoria:</h2>
         <CardCat />
       </div>
 
       <div className="my-18">
-        <h2 className="text-[32px] text-center font-medium">Produtos que estão bombando!</h2>
+        <h2 className="lg:text-[32px] md:text-2xl sm:text-xl text-center font-medium">Produtos que estão bombando!</h2>
         <CardProduct />
       </div>
 
