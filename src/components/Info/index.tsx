e9146d85281122bee5d89bf99487670da805cd9f
Jéssica Motta <jessicalimamotta@gmail.com>
2025-04-02 16:27:17 -0300

    adjust responsiveness in newsletter section

--- src/components/Info/index.tsx
+++ src/components/Info/index.tsx
@@ -3,26 +3,26 @@ import Image from 'next/image'
 export function Info() {
     return (
         <div className="bg-black w-full w-min-[238px] mt-20 mb-16 py-10 text-white">
-            <h2 className="font-medium lg:text-[32px] md:text-2xl sm:text-xl text-center">Conheça todas as nossas facilidades</h2>
+            <h2 className="font-medium md:text-[32px] text-[28px] text-center">Conheça todas as nossas facilidades</h2>
             {/* Seção com as informações */}
             <div className='flex flex-row flex-wrap justify-center gap-8 mt-10'>
                 {/* cada seção */}
                 <div className='flex justify-start items-center space-y-4 max-w-[304px]'>
-                    <Image src="/pix.svg" width={72} height={72} alt='Logo do PIX' className='mr-6'/>
+                    <Image src="/pix.svg" width={42} height={42} alt='Logo do PIX' className='mr-6 lg:w-[72px] lg:h-[72px]'/>
                     <div className='max-w-[200px]'>
                         <p className="uppercase text-(--color-primary) font-bold text-base">Pague pelo PIX</p>
                         <p>Ganhe 5% OFF em pagamentos via PIX</p>
                     </div>
                 </div>
                 <div className='flex justify-start items-center space-y-4 max-w-[304px]'>
-                    <Image src="/change.svg" width={72} height={72} alt='Logo do de troca' className='mr-6' />
+                    <Image src="/change.svg" width={42} height={42} alt='Logo do de troca' className='mr-6 lg:w-[72px] lg:h-[72px]' />
                     <div className='max-w-[200px]'>
                         <p className="uppercase text-(--color-primary) font-bold text-base">Troca grátis</p>
                         <p>Fique livre para trocar em até 30 dias</p>
                     </div>
                 </div>
                 <div className='flex justify-start items-center space-y-4 max-w-[304px]'>
-                    <Image src="/sustainability.svg" width={72} height={72} alt='Logo da sustentabilidade' className='mr-6' />
+                    <Image src="/sustainability.svg" width={42} height={42} alt='Logo da sustentabilidade' className='mr-6 lg:w-[72px] lg:h-[72px]' />
                     <div className='max-w-[200px]'>
                         <p className="uppercase text-(--color-primary) font-bold text-base">Sustentabilidade</p>
                         <p>Moda responsável, que respeita o meio ambiente</p>
--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -7,11 +7,11 @@ import Form from "next/form";
 export function Newsletter() {
     return (
         <div className="w-full my-[60px]">
-            <div className="border-2 border-black max-w-[730px] px-4 py-10 text-center mx-auto">
-                <p className="text-xl pb-6">Quer receber nossas novidades, promoções exclusivas e 10% OFF na primeira compra? <strong>Cadastre-se! </strong></p>
-                <Form action="/">
+            <div className="border-2 border-black max-w-[320px] md:max-w-[700px] px-4 py-10 text-center mx-auto">
+                <p className="text-base md:text-xl px-2 md:px-14">Quer receber nossas novidades, promoções exclusivas e 10% OFF na primeira compra? <strong>Cadastre-se! </strong></p>
+                <Form action="/" className="flex flex-row justify-center items-center px-2 md:px-16 py-6">
                     <input type="text" placeholder="Digite seu e-mail" 
-                    className="border-1 border-black lg:w-[456px] md:w-[350px] sm:w-[200px] placeholder:text-(--color-gray) py-2 px-4"
+                    className="border-1 border-black  placeholder:text-(--color-gray) py-2 px-4 w-full "
                     />
                     <Button>Enviar</Button>
                 </Form>
