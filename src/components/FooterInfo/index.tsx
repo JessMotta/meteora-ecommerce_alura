2d4a9d18ec0f32796f2aa84b299b6ff35c974756
Jéssica Motta <jessicalimamotta@gmail.com>
2025-04-02 17:00:10 -0300

    adjust menu

--- src/components/FooterInfo/index.tsx
+++ src/components/FooterInfo/index.tsx
@@ -1,6 +1,6 @@
 export function FooterInfo(){
     return (
-        <footer className="bg-black text-white text-center py-5 w-full h-[54px] flex justify-center items-center sm:text-sm">
+        <footer className="bg-black text-white text-center py-5 w-full h-[54px] flex justify-center items-center text-sm">
             <p>2025 © Desenvolvido por Alura | Projeto fictício sem fins comerciais.</p>
         </footer>
     )
