1bc29359d830f2b04d14933c35e5c2298fe0017a
Jéssica Motta <jessicalimamotta@gmail.com>
2025-04-03 11:58:32 -0300

    adjust header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,6 @@
+'use client';
 import Link from "next/link";
 import Image from "next/image";
-<<<<<<< Updated upstream
-
-
-const itemsMenu = [
-    { title: "Home", link: "/" }, 
-    { title: "Nossas Lojas", link: "/" }, 
-    { title: "Novidades", link: "/" }, 
-    { title: "Promoções", link: "/" }
-=======
 import { FaBars, FaXmark } from "react-icons/fa6";
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
@@ -21,22 +13,11 @@ const itemsMenu = [
     { title: "Nossas Lojas", link: "/store" },
     { title: "Novidades", link: "/news" },
     { title: "Promoções", link: "/onsale" }
->>>>>>> Stashed changes
 ];
 
 
 
 export default function Header() {
-<<<<<<< Updated upstream
-    return (
-        <header className="h-[54px] bg-black text-white flex justify-between items-center px-5">
-            <div className="flex items-center justify-start">
-                <Link href="/">
-                    <Image src="/logo.svg" alt="Logo" width={200} height={54} />
-                </Link>
-                <nav className="pl-10">
-                    <ul className="flex justify-start space-x-4">
-=======
 
     const [openMenu, setOpenMenu] = useState<boolean>(false);
 
@@ -62,21 +43,12 @@ export default function Header() {
                         <Image src="/logo.svg" alt="Logo" width={200} height={54} />
                     </Link>
                     <ul className={`lg:flex justify-start items-center space-x-4 lg:pl-10 pl-6 px-6 ${openMenu ? "block absolute top-0 right-0 w-fit h-auto py-3 bg-gray-mobile pt-10" : "hidden"}`}>
->>>>>>> Stashed changes
                         {itemsMenu.map((item, index) => (
                             <li key={index} className={`lg:pl-5 text-white font-normal ${openMenu ? "pl-0 py-2 border-b-2 border-gray-mobile2" : "pl-5"} ${pathname === item.link ? "text-primary font-bold" : ""}`} >
                                 <Link href={item.link}>{item.title}</Link>
                             </li>
                         ))}
                     </ul>
-<<<<<<< Updated upstream
-                </nav>
-            </div>
-            <div className="flex justify-end items-center space-x-4">
-                <input type="text" placeholder="Digite o produto" className="bg-white text-black placeholder-(--color-gray) py-2 px-3"></input>
-                <Button color="transparent">Buscar</Button>
-            </div>
-=======
                 </div>
 
                 <SearchBar button="Buscar" show={searchBar} menuPosition={true} />
@@ -84,7 +56,6 @@ export default function Header() {
                     {openMenu ? <FaXmark className="fill-primary" size={24} /> : <FaBars className="fill-primary" size={24} />}
                 </button>
             </nav>
->>>>>>> Stashed changes
         </header>
     )
 }
