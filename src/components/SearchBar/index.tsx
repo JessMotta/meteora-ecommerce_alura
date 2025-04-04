999e86e3d2d585770a9c6f7c802fcc40a6c0543c
Jéssica Motta <jessicalimamotta@gmail.com>
2025-04-04 17:49:34 -0300

    fix button

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -13,7 +13,7 @@ export function SearchBar(props: SearchBarInterface) {
     return (
         <div className={`space-x-4 justify-center items-center ${props.show ? "flex" : "hidden"}  `}>
             <input type="text" placeholder="Digite o produto" className= {`py-2 px-3  ${props.menuPosition ? menu : notMenu}`}></input>
-            <Button color={props.menuPosition ? 'transparent': 'black'}>{props.button}</Button>
+            <Button color={props.menuPosition ? 'transparent': 'black'} onClickFunction={() => {}}>{props.button}</Button>
         </div>
     )
 }
