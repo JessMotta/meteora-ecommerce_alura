3a386ce2e13af02061cc8251fe040b461fbffb08
Jéssica Motta <jessicalimamotta@gmail.com>
2025-04-10 11:37:25 -0300

    add info to other pages

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,10 +1,11 @@
-import DialogProduct from "@/components/DialogProduct";
+import { LuConstruction } from "react-icons/lu";
+
 
 export default function News() {
   return (
-    <>
-      <div>Página sobre as novidades</div>
-      <DialogProduct/>
-    </>
+    <div className="flex flex-col items-center justify-center h-screen text-center">
+    <LuConstruction className="text-6xl text-secondary" />
+      <h1 className="text-2xl mt-8 text-gray">Em breve <br/> Página sobre as novidades da Meteora</h1>
+    </div>
   )
 }
